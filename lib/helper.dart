1c41bd9b37d8afe0126bcf25f485632b04935979
Phi Le <ple31@student.gsu.edu>
2025-03-05 14:04:22 -0500

    Populate the database tables

--- lib/helper.dart
+++ lib/helper.dart
@@ -33,7 +33,28 @@ class DatabaseHelper {
       image TEXT,
       folder_id INTEGER,
       FOREIGN KEY (folder_id) REFERENCES Folders(id)
-      );  
+      );
+
+      INSERT INTO Folders (id, name, time) VALUES
+      (1, 'Hearts', CURRENT_TIMESTAMP),
+      (2, 'Spades', CURRENT_TIMESTAMP),
+      (3, 'Clubs', CURRENT_TIMESTAMP),
+      (4, 'Diamonds', CURRENT_TIMESTAMP);
+
+      INSERT INTO Cards (name, suit, image, folder_id) VALUES
+      ('Ace', 'Hearts', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/01_of_hearts_A.svg/800px-01_of_hearts_A.svg.png', 1),
+      ('2', 'Hearts', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/02_of_hearts.svg/800px-02_of_hearts.svg.png', 1),
+      ('3', 'Hearts', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/03_of_hearts.svg/800px-03_of_hearts.svg.png', 1),
+      ('4', 'Spades', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/04_of_spades.svg/800px-04_of_spades.svg.png', 2),
+      ('5', 'Spades', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/05_of_spades.svg/800px-05_of_spades.svg.png', 2),
+      ('6', 'Spades', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/06_of_spades.svg/800px-06_of_spades.svg.png', 2),
+      ('7', 'Clubs', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/07_of_clubs.svg/800px-07_of_clubs.svg.png', 3),
+      ('8', 'Clubs', 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/08_of_clubs.svg/800px-08_of_clubs.svg.png', 3),
+      ('9', 'Clubs', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/09_of_clubs.svg/800px-09_of_clubs.svg.png', 3),
+      ('10', 'Diamond', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/10_of_diamonds_-_David_Bellot.svg/232px-10_of_diamonds_-_David_Bellot.svg.png', 4),
+      ('Jack', 'Diamond', 'https://upload.wikimedia.org/wikipedia/commons/a/aa/Poker-sm-234-Jd.png', 4),
+      ('Queen', 'Diamond', 'https://upload.wikimedia.org/wikipedia/commons/7/70/Poker-sm-233-Qd.png', 4),
+      ('King', 'Diamond', 'https://upload.wikimedia.org/wikipedia/commons/d/d0/Poker-sm-232-Kd.png', 4);
       ''');
   }
 
